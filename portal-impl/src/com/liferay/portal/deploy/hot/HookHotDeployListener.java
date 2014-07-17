de0dd5e8bf377e46537954a10d21642eba7120e2
peterfellwock <peter.fellwock@liferay.com>
2014-07-17 10:22:31 -0700

    LPS-48592 DefaultControlPanelEntryFactory Registry'fy

--- portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
+++ portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
@@ -135,7 +135,6 @@ import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.PropsUtil;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.portlet.ControlPanelEntry;
-import com.liferay.portlet.DefaultControlPanelEntryFactory;
 import com.liferay.portlet.assetpublisher.util.AssetEntryQueryProcessor;
 import com.liferay.portlet.assetpublisher.util.AssetPublisherUtil;
 import com.liferay.portlet.documentlibrary.antivirus.AntivirusScanner;
@@ -491,12 +490,6 @@ public class HookHotDeployListener
 			}
 
 			captchaImpl.setCaptcha(null);
-		}
-
-		if (portalProperties.containsKey(
-				PropsKeys.CONTROL_PANEL_DEFAULT_ENTRY_CLASS)) {
-
-			DefaultControlPanelEntryFactory.setInstance(null);
 		}
 
 		if (portalProperties.containsKey(PropsKeys.DL_FILE_ENTRY_PROCESSORS)) {
@@ -1621,7 +1614,8 @@ public class HookHotDeployListener
 					portletClassLoader, ControlPanelEntry.class,
 					controlPanelEntryClassName);
 
-			DefaultControlPanelEntryFactory.setInstance(controlPanelEntry);
+			registerService(servletContextName, controlPanelEntryClassName,
+				ControlPanelEntry.class, controlPanelEntry);
 		}
 
 		if (portalProperties.containsKey(PropsKeys.DL_FILE_ENTRY_PROCESSORS)) {
