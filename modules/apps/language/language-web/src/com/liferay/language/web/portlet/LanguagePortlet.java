c0b0d612e2fef75a3dcc3df8b76a989867eea6aa
peterfellwock <peter.fellwock@liferay.com>
2014-09-24 16:30:15 -0700

    LPS-50130 New Portlet class

---
+++ modules/apps/language/language-web/src/com/liferay/language/web/portlet/LanguagePortlet.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.language.web.portlet;
+
+import com.liferay.language.web.upgrade.LanguageUpgrade;
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Peter Fellwock
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.css-class-wrapper=portlet-language",
+		"com.liferay.portlet.display-category=category.tools",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.icon=/icons/language.png",
+		"com.liferay.portlet.layout-cacheable=true",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.remoteable=true",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.struts-path=language",
+		"com.liferay.portlet.use-default-template=true",
+		"com.liferay.portlet.configuration-path=/configuration.jsp",
+		"javax.portlet.display-name=Language",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=guest,power-user,user"
+	},
+	service = Portlet.class
+)
+public class LanguagePortlet extends MVCPortlet {
+
+	@Reference(unbind = "-")
+	protected void setLanguageUpgrade(LanguageUpgrade languageUpgrade) {
+	}
+}
