bc4f394cfa18e727c8fa304e1c80fd9844459d68
peterfellwock <peter.fellwock@liferay.com>
2015-01-06 16:39:38 -0800

    LPS-51814 (Template OSGI) As OSGI leaves, TemplateManagers do their own destroy

--- portal-impl/src/com/liferay/portal/events/GlobalShutdownAction.java
+++ portal-impl/src/com/liferay/portal/events/GlobalShutdownAction.java
@@ -194,21 +194,6 @@ public class GlobalShutdownAction extends SimpleAction {
 			e.printStackTrace();
 		}
 
-		// Template manager
-
-		try {
-			TemplateManagerUtil.destroy();
-		}
-		catch (Exception e) {
-		}
-
-		// Template resource loader
-
-		try {
-			TemplateResourceLoaderUtil.destroy();
-		}
-		catch (Exception e) {
-		}
 	}
 
 	protected void shutdownLevel2() {
