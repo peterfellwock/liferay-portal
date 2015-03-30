beb09a45cac4678b27c74bebfea2253891388afa
peterfellwock <peter.fellwock@liferay.com>
2015-03-30 14:32:10 -0700

    LPS-54505 - Expand PortalWebResource to handle multiple services

--- portal-service/src/com/liferay/portal/kernel/web/PortalWebResourcesUtil.java
+++ portal-service/src/com/liferay/portal/kernel/web/PortalWebResourcesUtil.java
@@ -16,7 +16,16 @@ package com.liferay.portal.kernel.web;
 
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceReference;
 import com.liferay.registry.ServiceTracker;
+import com.liferay.registry.ServiceTrackerCustomizer;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.ServletContext;
 
@@ -25,26 +34,69 @@ import javax.servlet.ServletContext;
  */
 public class PortalWebResourcesUtil {
 
-	public static String getContextPath() {
-		return getPortalWebResources().getContextPath();
+	public static String getContextPath(String resourceType) {
+		return getPortalWebResources(resourceType).getContextPath();
 	}
 
-	public static long getLastModified() {
-		return getPortalWebResources().getLastModified();
+	public static long getLastModified(String resourceType) {
+		return getPortalWebResources(resourceType).getLastModified();
 	}
 
-	public static PortalWebResources getPortalWebResources() {
-		return _instance._serviceTracker.getService();
+	public static PortalWebResources getPortalWebResources(
+		String resourceType) {
+		return _instance._portalWebResourcesMap.get(resourceType);
 	}
 
-	public static ServletContext getServletContext() {
-		return getPortalWebResources().getServletContext();
+	public static ServletContext getServletContext(String resourceType) {
+		return getPortalWebResources(resourceType).getServletContext();
+	}
+
+	public static boolean isResourceContextPath(String requestURI) {
+
+		for (PortalWebResources portalWebResources :
+			_instance._getPortalWebResourcesList() ) {
+
+			if (requestURI.startsWith(portalWebResources.getContextPath())) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static URL getServletContextResource(String resourceName) {
+
+		for (PortalWebResources portalWebResources :
+			_instance._getPortalWebResourcesList() ) {
+
+			ServletContext servletContext =
+				portalWebResources.getServletContext();
+
+			try {
+				URL url = servletContext.getResource(resourceName);
+
+				if (url != null) {
+					return url;
+				}
+			}
+			catch (MalformedURLException e) {
+			}
+		}
+
+		return null;
+	}
+
+	private Collection<PortalWebResources> _getPortalWebResourcesList() {
+
+		return _portalWebResourcesMap.values();
 	}
 
 	private PortalWebResourcesUtil() {
 		Registry registry = RegistryUtil.getRegistry();
 
-		_serviceTracker = registry.trackServices(PortalWebResources.class);
+		_serviceTracker = registry.trackServices(
+			PortalWebResources.class,
+			new PortalWebResourcesServiceTrackerCustomizer());
 
 		_serviceTracker.open();
 	}
@@ -54,5 +106,51 @@ public class PortalWebResourcesUtil {
 
 	private final ServiceTracker<PortalWebResources, PortalWebResources>
 		_serviceTracker;
+
+	private final Map<String, PortalWebResources>
+	_portalWebResourcesMap = new ConcurrentHashMap<>();
+
+	private class PortalWebResourcesServiceTrackerCustomizer
+	implements ServiceTrackerCustomizer<PortalWebResources, PortalWebResources> {
+
+		@Override
+		public PortalWebResources addingService(
+			ServiceReference<PortalWebResources> serviceReference) {
+
+			Registry registry = RegistryUtil.getRegistry();
+
+			PortalWebResources portalWebResources = registry.getService(
+				serviceReference);
+
+			String resourceType = portalWebResources.getResourceType();
+
+			System.out.println(
+				"\n\n\n\n------------------------------------------------------------------------------------------------------------------");
+			System.out.println(
+				"Registering PortalWebResource: " + resourceType);
+
+			_portalWebResourcesMap.put(resourceType, portalWebResources);
+
+			return portalWebResources;
+		}
+
+		@Override
+		public void modifiedService(
+			ServiceReference<PortalWebResources> serviceReference,
+			PortalWebResources portalWebResources) {
+		}
+
+		@Override
+		public void removedService(
+			ServiceReference<PortalWebResources> serviceReference,
+			PortalWebResources portalWebResources) {
+
+			Registry registry = RegistryUtil.getRegistry();
+
+			registry.ungetService(serviceReference);
+
+			_portalWebResourcesMap.remove(portalWebResources.getResourceType());
+		}
+	}
 
 }
