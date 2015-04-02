08b37686d7c525cd5c0ba817b552fd51108ce02a
michael.bradford@liferay.com <michael.bradford@liferay.com>
2015-04-02 16:09:49 -0700

    LPS-54505 - Adjust replacement base url for substitutions

--- portal-impl/src/com/liferay/portal/servlet/filters/dynamiccss/DynamicCSSUtil.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/dynamiccss/DynamicCSSUtil.java
@@ -37,6 +37,7 @@ import com.liferay.portal.service.ThemeLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.tools.SassToCssBuilder;
 import com.liferay.portal.util.ClassLoaderUtil;
+import com.liferay.portal.util.PortalImpl;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.sass.compiler.jni.JniSassCompiler;
@@ -269,8 +270,8 @@ public class DynamicCSSUtil {
 		String contextPath = ContextPathUtil.getContextPath(servletContext);
 
 		if (!contextPath.equals(portalContextPath)) {
-			baseURL = StringPool.SLASH.concat(
-				GetterUtil.getString(servletContext.getServletContextName()));
+			baseURL = PortalImpl.PATH_MODULE.concat(
+				GetterUtil.getString(StringPool.SLASH + servletContext.getServletContextName()));
 		}
 
 		if (baseURL.endsWith(StringPool.SLASH)) {
