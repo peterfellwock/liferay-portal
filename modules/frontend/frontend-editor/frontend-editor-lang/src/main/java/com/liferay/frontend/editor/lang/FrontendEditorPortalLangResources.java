d456405a6ac27c3b90d0dfd0e96ff53f2b5f975b
michael.bradford@liferay.com <michael.bradford@liferay.com>
2015-10-30 13:23:54 -0700

    LPS-56537 - create FrontendEditorLang class for editor PortalWebResources classes to reference

---
+++ modules/frontend/frontend-editor/frontend-editor-lang/src/main/java/com/liferay/frontend/editor/lang/FrontendEditorPortalLangResources.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editor.lang;
+
+import com.liferay.portal.kernel.servlet.PortalLangResources;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Michael Bradford
+ */
+@Component(immediate = true, service = PortalLangResources.class)
+public class FrontendEditorPortalLangResources implements PortalLangResources {
+
+	public Class getLangResourceBundleClass() {
+		return getClass();
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundle = bundleContext.getBundle();
+	}
+
+	private Bundle _bundle;
+
+}
