0d330a2bb167ab654e2be9558483472e77c68390
drewbrokke <drew.brokke@liferay.com>
2016-02-08 16:55:29 -0800

    LPS-56042 contacts-web - registers ContactsCenterActivityInterpreter

--- modules/apps/platform/contacts/contacts-web/src/main/java/com/liferay/contacts/web/social/ContactsCenterActivityInterpreter.java
+++ modules/apps/platform/contacts/contacts-web/src/main/java/com/liferay/contacts/web/social/ContactsCenterActivityInterpreter.java
@@ -17,6 +17,7 @@
 
 package com.liferay.contacts.web.social;
 
+import com.liferay.contacts.constants.ContactsPortletKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.util.ClassResourceBundleLoader;
 import com.liferay.portal.kernel.util.ResourceBundleLoader;
@@ -25,11 +26,18 @@ import com.liferay.portal.model.User;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.social.kernel.model.BaseSocialActivityInterpreter;
 import com.liferay.social.kernel.model.SocialActivity;
+import com.liferay.social.kernel.model.SocialActivityInterpreter;
 import com.liferay.social.kernel.model.SocialRelationConstants;
+
+import org.osgi.service.component.annotations.Component;
 
 /**
  * @author Hai Yu
  */
+@Component(
+	property = {"javax.portlet.name=" + ContactsPortletKeys.CONTACTS_CENTER},
+	service = SocialActivityInterpreter.class
+)
 public class ContactsCenterActivityInterpreter
 	extends BaseSocialActivityInterpreter {
 
