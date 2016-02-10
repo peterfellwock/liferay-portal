9c7124bec1b322af2d3dbdac7af9ef3219395dfb
drewbrokke <drew.brokke@liferay.com>
2016-02-10 15:21:38 -0800

    LPS-56042 contacts-web - registers MembersPortlet as a component

---
+++ modules/apps/platform/contacts/contacts-web/src/main/java/com/liferay/contacts/web/portlet/MembersPortlet.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This file is part of Liferay Social Office. Liferay Social Office is free
+ * software: you can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Liferay Social Office is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Liferay Social Office. If not, see http://www.gnu.org/licenses/agpl-3.0.html.
+ */
+
+package com.liferay.contacts.web.portlet;
+
+import com.liferay.contacts.constants.ContactsPortletKeys;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Drew Brokke
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.css-class-wrapper=contacts-portlet",
+		"com.liferay.portlet.display-category=category.social",
+		"com.liferay.portlet.friendly-url-mapping=contacts",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.header-portlet-javascript=/js/main.js",
+		"com.liferay.portlet.icon=/icon.png",
+		"javax.portlet.display-name=Members",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.info.keywords=Members",
+		"javax.portlet.info.short-title=Members",
+		"javax.portlet.info.title=Members",
+		"javax.portlet.init-param.config-template=/configuration.jsp",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + ContactsPortletKeys.MEMBERS,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator,guest,power-user,user",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class MembersPortlet extends ContactsCenterPortlet {
+}
