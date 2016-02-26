d8a270f6407c3ccbc3f781cc17a624c391a20f54
drewbrokke <drew.brokke@liferay.com>
2016-02-26 13:20:02 -0800

    LPS-56042 contacts-web - registers upgrade process

---
+++ modules/apps/platform/contacts/contacts-web/src/main/java/com/liferay/contacts/web/upgrade/ContactsWebUpgrade.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This file is part of Liferay Social Office. Liferay Social Office is free
+ * software: you can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Liferay Social Office is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Liferay Social Office. If not, see http://www.gnu.org/licenses/agpl-3.0.html.
+ */
+
+package com.liferay.contacts.web.upgrade;
+
+import com.liferay.contacts.web.upgrade.v1_0_0.UpgradePortletId;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Drew Brokke
+ */
+@Component(immediate = true, service = UpgradeStepRegistrator.class)
+public class ContactsWebUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.contacts.web", "0.0.1", "1.0.0",
+			new UpgradePortletId());
+	}
+
+}
