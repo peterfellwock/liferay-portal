880cc7dc1c3afd52325c23bc2a3f6c796e2477d8
Michael Han <michael.han@liferay.com>
2016-03-17 15:18:59 -0500

    LPS-64288 update repackage

--- modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/SearchPermissionCheckerImpl.java
+++ modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/SearchPermissionCheckerImpl.java
@@ -50,8 +50,8 @@ import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.search.FieldNames;
 import com.liferay.portal.search.configuration.SearchPermissionCheckerConfiguration;
+import com.liferay.portal.search.constants.FieldNames;
 
 import java.util.ArrayList;
 import java.util.Collections;
