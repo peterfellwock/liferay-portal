6ce3163c434d276da95fd86672a13bda22e846ff
peterfellwock <peter.fellwock@liferay.com>
2016-04-04 15:26:39 -0700

    LPS-64871 Update java package

--- modules/apps/foundation/petra/petra-pkparser-util/src/main/java/com/liferay/petra/pkparser/util/PKParser.java
+++ modules/apps/foundation/petra/petra-pkparser-util/src/main/java/com/liferay/petra/pkparser/util/PKParser.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.util;
+package com.liferay.petra.pkparser.util;
 
 import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.GetterUtil;
