08c9b1c8d37d6739520184dcbb1a2343e49c9014
peterfellwock <peter.fellwock@liferay.com>
2016-09-08 16:03:54 -0700

    LPS-12345 Print all exception from highest to collision

--- portal-impl/src/com/liferay/portal/util/IdExCatch.java
+++ portal-impl/src/com/liferay/portal/util/IdExCatch.java
@@ -21,18 +21,66 @@ public class IdExCatch {
 			highest = ex.getMVCC();
 		}else{
 			System.out.println("-------------------------->SOMEONE IS TRYING TO ADD MVCC STALE!!!!!");
-			System.out.println("-INCOMING-");
+			System.out.println("-------------------------->SOMEONE IS TRYING TO ADD MVCC STALE!!!!!");
+			System.out.println("Highest mvvcc for ID:" + ex.getKey() + " is:" + highest);
+			
+			
+			System.out.println("-------------------Printing all ExCatches from collision");
+			for(long x = ex.getMVCC() ; x <= highest ; x++){
+				
+				ExCatch temp = catches.get(x+ "");
+				if(temp == null){
+					System.out.println("->Could not find ExCatch with mvcc" + x);
+				}else{
+					temp.print();
+				}
+			}
+			System.out.println("+ colliding ExCatch");
+			ex.print();
+			System.out.println("-------------------------------------------------------------------------");
+			System.out.println("-------------------Printing all ExCatches from collision with Exceptions");
+			for(long x = ex.getMVCC() ; x <= highest ; x++){
+				
+				ExCatch temp = catches.get(x+ "");
+				if(temp == null){
+					System.out.println("->Could not find ExCatch with mvcc" + x);
+				}else{
+					temp.print();
+					temp.printEx();
+				}
+				System.out.println("-----------!!!!!!!!!!!!!!!!!!!!!!NEXT");
+			}
+			System.out.println("+ colliding ExCatch with Exception");
 			ex.print();
 			ex.printEx();
-			System.out.println("-VS-");
-			catches.get(ex.getStringMVCC()).print();
-			catches.get(ex.getStringMVCC()).printEx();
+			System.out.println("-------------------------------------------------------------------------");
+
 		}
 	}
 	
 	public long getHighest(){
 		return highest;
 	}
+	
+	
+	public static void main(String[] args){
+
+		ExCatch catch53 = new ExCatch(new Exception(), "111", 53, "number53", 666);
+		ExCatch catch54 = new ExCatch(new Exception(), "111", 54, "number54", 666);
+		ExCatch catch55 = new ExCatch(new Exception(), "111", 55, "number55", 666);
+		ExCatch catch56 = new ExCatch(new Exception(), "111", 56, "number56", 666);
+		ExCatch catch57 = new ExCatch(new Exception(), "111", 57, "number57", 666);
+		ExCatch catch58 = new ExCatch(new Exception(), "111", 58, "number58", 666);
+		ExCatch catch552 = new ExCatch(new Exception(), "111", 55, "number552", 666);
 		
+		ExCatchUtil.add(catch53);
+		ExCatchUtil.add(catch54);
+		ExCatchUtil.add(catch55);
+		ExCatchUtil.add(catch56);
+		ExCatchUtil.add(catch57);
+		ExCatchUtil.add(catch58);
+		ExCatchUtil.add(catch552);
+		
+	}
 
 }
