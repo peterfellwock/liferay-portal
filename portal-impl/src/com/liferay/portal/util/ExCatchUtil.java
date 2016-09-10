2d27a85e40c5c7f1bb3439fc8530529016ded2fb
peterfellwock <peter.fellwock@liferay.com>
2016-09-10 13:20:36 -0700

    LPS-12345 Cleanup and print all Catches

--- portal-impl/src/com/liferay/portal/util/ExCatchUtil.java
+++ portal-impl/src/com/liferay/portal/util/ExCatchUtil.java
@@ -1,47 +1,55 @@
 package com.liferay.portal.util;
 
 import java.util.HashMap;
-
 public class ExCatchUtil {
-	
-	private static ExCatchUtil instance;
-	private static HashMap<String, IdExCatch> catches;
-	
-	private ExCatchUtil(){
-		
-		catches = new HashMap<String, IdExCatch>();
-		
-	}
-	
-	public static ExCatchUtil getInstance(){
-		
-		if(instance == null){
-			instance = new ExCatchUtil();
-		}
-		
-		return instance;
-	}
-	
-	public static void add(ExCatch exCatch){
-		
-		if(contains(exCatch)){
+
+	public static void add(ExCatch exCatch) {
+		if (contains(exCatch)) {
 			IdExCatch existing = get(exCatch.getKey());
+
 			existing.put(exCatch);
-		}else{
+		}else {
 			IdExCatch newIdEx = new IdExCatch(exCatch);
 			getInstance().catches.put(exCatch.getKey(), newIdEx);
 			//System.out.println("SAVING:");
 		}
-		
-		//exCatch.print();
+
+		exCatch.print();
 	}
-	
-	public static boolean contains(ExCatch exCatch){
+
+	public static boolean contains(ExCatch exCatch) {
 		return getInstance().catches.containsKey(exCatch.getKey());
 	}
-	
-	public static IdExCatch get(String key){
+
+	public static void discover(Exception e) {
+		StackTraceElement[] stacks = e.getStackTrace();
+
+		for (StackTraceElement stack : stacks) {
+			String className = stack.getClassName();
+
+			if (className.equals("LayoutSetLocalServiceImpl")) {
+				//stack
+			}
+		}
+	}
+
+	public static IdExCatch get(String key) {
 		return getInstance().catches.get(key);
 	}
-	
+
+	public static ExCatchUtil getInstance() {
+		if (instance == null) {
+			instance = new ExCatchUtil();
+		}
+
+		return instance;
+	}
+
+	private ExCatchUtil() {
+		catches = new HashMap<>();
+	}
+
+	protected static HashMap<String, IdExCatch> catches;
+	private static ExCatchUtil instance;
+
 }
--- portal-impl/src/com/liferay/portal/util/IdExCatch.java
+++ portal-impl/src/com/liferay/portal/util/IdExCatch.java
@@ -1,86 +1,156 @@
 package com.liferay.portal.util;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.List;
 public class IdExCatch {
-	
-	private HashMap<String, ExCatch> catches;
+
+	private List<ExCatch> catches;
 	private long highest = 0;
-	
-	
+
 	public IdExCatch(ExCatch ex) {
-		super();
-		catches = new HashMap<String, ExCatch>();
-		catches.put(ex.getStringMVCC(), ex);
+		
+		catches = new ArrayList<ExCatch>();
+		catches.add(ex);
+		
+		highest = ex.getMVCC();
 	}
-	
-	public void put(ExCatch ex){
 
-		if(ex.getMVCC() >= highest){
-			catches.put(ex.getStringMVCC(), ex);
+	public void put(ExCatch ex) {
+
+		
+		/** if mvcc is same or higher save stack and move on **/
+		if (ex.getMVCC() >= highest) {
+			catches.add(ex);
 			highest = ex.getMVCC();
-		}else{
-			System.out.println("-------------------------->SOMEONE IS TRYING TO ADD MVCC STALE!!!!!");
-			System.out.println("-------------------------->SOMEONE IS TRYING TO ADD MVCC STALE!!!!!");
-			System.out.println("Highest mvvcc for ID:" + ex.getKey() + " is:" + highest);
+		}else {
 			
+			/** mvcc is lower than highest, prossible stale **/
 			
-			System.out.println("-------------------Printing all ExCatches from collision");
-			for(long x = ex.getMVCC() ; x <= highest ; x++){
-				
-				ExCatch temp = catches.get(x+ "");
-				if(temp == null){
-					System.out.println("->Could not find ExCatch with mvcc" + x);
-				}else{
-					temp.print();
-				}
-			}
-			System.out.println("+ colliding ExCatch");
-			ex.print();
-			System.out.println("-------------------------------------------------------------------------");
-			System.out.println("-------------------Printing all ExCatches from collision with Exceptions");
-			for(long x = ex.getMVCC() ; x <= highest ; x++){
-				
-				ExCatch temp = catches.get(x+ "");
-				if(temp == null){
-					System.out.println("->Could not find ExCatch with mvcc" + x);
-				}else{
-					temp.print();
-					temp.printEx();
-				}
-				System.out.println("-----------!!!!!!!!!!!!!!!!!!!!!!NEXT");
-			}
-			System.out.println("+ colliding ExCatch with Exception");
-			ex.print();
-			ex.printEx();
-			System.out.println("-------------------------------------------------------------------------");
+			/** header **/
+			printCollisionNotification(ex);
 
+			/** print summary **/
+			printSummary(ex);
+
+			/** print exceptions **/
+			printExceptions(ex);
 		}
 	}
+
+	private void printSummary(ExCatch challenger) {
+
+		System.out.println(
+				"---------------------------SUMMARY-----------------------------");
+		
+		/** controls **/
+		long endMvccId = this.highest;
+		/** loop backwards till incoming -1**/
+		long stopMvccId = challenger.getMVCC() -1;
+		boolean keepPrinting = true;
+		int position = catches.size() -1;
+		
+		while(keepPrinting) {
+			ExCatch temp = catches.get(position);
+			
+			if(stopMvccId > temp.getMVCC()){
+				keepPrinting = false;
+			}else{
+				temp.print();
+			}
+			position--;
+
+		}
+		System.out.println("Challenger::");
+		challenger.print();
+		System.out.println(
+				"------------------------END OF SUMMARY--------------------------");
+		
+	}
 	
-	public long getHighest(){
+	private void printExceptions(ExCatch challenger) {
+
+		System.out.println(
+			"---------------------SUMMARY of EXCEPTIONS-----------------------");
+		
+		/** controls **/
+		long endMvccId = this.highest;
+		/** loop backwards till incoming -1**/
+		long stopMvccId = challenger.getMVCC() -1;
+		boolean keepPrinting = true;
+		int position = catches.size() -1;
+		
+		while(keepPrinting) {
+			ExCatch temp = catches.get(position);
+			
+			if(stopMvccId > temp.getMVCC()){
+				keepPrinting = false;
+			}else{
+				temp.print();
+				temp.printEx();
+				System.out.println("---------------------------------NEXT!");
+			}
+			position--;
+
+
+		}
+		System.out.println("Challenger::");
+		challenger.print();
+		System.out.println(
+			"-------------------END SUMMARY of EXCEPTIONS---------------------");
+		
+	}
+
+	public long getHighest() {
 		return highest;
 	}
 	
-	
-	public static void main(String[] args){
+	public void printCollisionNotification(ExCatch challenger){
+		System.out.println(
+				"\n\n--------------------------------------------------------------------");
+			System.out.println(
+				"-------Possible collision-----------------------------------------------");
+			System.out.println(
+				"Highest mvvcc for ID:" + challenger.getKey() + " is:" + highest);
 
-		ExCatch catch53 = new ExCatch(new Exception(), "111", 53, "number53", 666);
-		ExCatch catch54 = new ExCatch(new Exception(), "111", 54, "number54", 666);
-		ExCatch catch55 = new ExCatch(new Exception(), "111", 55, "number55", 666);
-		ExCatch catch56 = new ExCatch(new Exception(), "111", 56, "number56", 666);
-		ExCatch catch57 = new ExCatch(new Exception(), "111", 57, "number57", 666);
-		ExCatch catch58 = new ExCatch(new Exception(), "111", 58, "number58", 666);
-		ExCatch catch552 = new ExCatch(new Exception(), "111", 55, "number552", 666);
-		
+			System.out.println(
+				"-------------------Printing all ExCatches from collision");
+	}
+	
+
+	public static void main(String[] args) {
+
+		ExCatch catch52 = new ExCatch(
+				new Exception(), "111", 53, "number53", 666);
+		ExCatch catch53 = new ExCatch(
+			new Exception(), "111", 53, "number53", 666);
+		ExCatch catch54 = new ExCatch(
+			new Exception(), "111", 54, "number54", 666);
+		ExCatch catch55 = new ExCatch(
+			new Exception(), "111", 55, "number55", 666);
+		ExCatch catch56 = new ExCatch(
+			new Exception(), "111", 56, "number56", 666);
+		ExCatch catch566 = new ExCatch(
+				new Exception(), "111", 56, "Anothernumber56", 666);
+		ExCatch catch5666 = new ExCatch(
+				new Exception(), "111", 56, "Anothernumber566", 666);
+		ExCatch catch57 = new ExCatch(
+			new Exception(), "111", 57, "number57", 666);
+		ExCatch catch58 = new ExCatch(
+			new Exception(), "111", 58, "number58", 666);
+		ExCatch catch552 = new ExCatch(
+			new Exception(), "111", 55, "number552", 666);
+
+		ExCatchUtil.add(catch52);
 		ExCatchUtil.add(catch53);
 		ExCatchUtil.add(catch54);
 		ExCatchUtil.add(catch55);
 		ExCatchUtil.add(catch56);
+		ExCatchUtil.add(catch566);
+		ExCatchUtil.add(catch5666);
 		ExCatchUtil.add(catch57);
 		ExCatchUtil.add(catch58);
 		ExCatchUtil.add(catch552);
-		
 	}
 
 }
